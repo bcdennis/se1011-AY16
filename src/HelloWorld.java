2562466fe06a1bb63638e552b82a5a3ce22d8fed
Brad Dennis <bcdennis@gmail.com>
2015-09-09 10:05:00 -0500

    adding helloworld

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * HelloWorld.java
+ *
+ * This is a simple 'hello world' program used in SE1011
+ * to demonstrate some first Java programming concepts.
+ *
+ * @author Brad Dennis
+ * Created on:  09/09/2015
+ * Last updated: 09/09/2015
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
