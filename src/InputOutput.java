fbd70d2370ca504d0357eb7bdfd04b4f9bbc3b6e
Brad Dennis <bcdennis@gmail.com>
2015-09-17 08:48:41 -0500

    adding 9-17

---
+++ src/InputOutput.java
@@ -0,0 +1,56 @@
+/**
+ * InputOutput.java
+ *
+ * This is a simple program that will be used to demonstrate
+ * more Input Output.
+ *
+ * @author Brad Dennis
+ * Created:  09/17/2015
+ * Modified: --
+ */
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+public class InputOutput {
+
+    public static void main(String[] args) {
+        // Variable declarations
+        Scanner in = new Scanner(System.in);
+        int integer;
+        long longInteger;
+        float realNumber;
+        double doubleReal;
+        String string1;
+        String string2;
+
+        // Prompts for user input
+        System.out.println("Enter an integer, a long integer, "
+                + "a floating-point ");
+        System.out.println("number, another floating-point number, "
+                + "and a string.");
+        System.out.println("Separate each with a blank or return.");
+
+        // Read in values
+        integer = in.nextInt();
+        longInteger = in.nextLong();
+        realNumber = in.nextFloat();
+        doubleReal = in.nextDouble();
+        string1 = in.nextLine();
+        System.out.println("Now enter another value.");
+        string2 = in.next();
+
+        System.out.println("Here is what you entered: ");
+        System.out.println(integer + " " + longInteger + " " + realNumber +
+                " " + doubleReal + " " + string1 +
+                " and " + string2);
+
+        // We can also interact with the user via a GUI
+        String input = JOptionPane.showInputDialog("Please enter a number");
+
+        int userValue = Integer.parseInt(input);
+        int newValue = userValue * 10;
+
+
+        JOptionPane.showMessageDialog(null, "Your number times ten is equal to " + newValue);
+    }
+}
---
+++ src/WrapperClasses.java
@@ -0,0 +1,52 @@
+/**
+ * WrapperClasses.java
+ *
+ * This is a simple program that will be used to demonstrate
+ * Wrapper Classes.
+ *
+ * @author Brad Dennis
+ * Created:  09/17/2015
+ * Modified: --
+ */
+public class WrapperClasses {
+
+    public static void main(String[] args) {
+        //Casting Review
+
+        // declaration
+        double examWeight;
+        double examAverage;
+        double labWeight;
+        double labAverage;
+
+        // initialization
+        examWeight = .60;
+        examAverage = 88;   //What type of casting is this?
+
+        labWeight = .40;
+        labAverage = 93;    // and this?
+
+        // What's wrong with this?
+        //int finalGrade = (int) examWeight * examAverage + labWeight * labAverage;
+
+        // declaration and initialization
+        int exam1 = 90;
+        int exam2 = 84;
+        int total = exam1 + exam2;
+
+        // Why does this work?
+        double average = (double) total/2;
+
+        // What do we do if our values are in Strings?
+        String examOne = "90";
+        String examTwo = "84";
+
+        //total = examOne + examOne;
+        //total = (int) (examOne + examTwo);
+
+        total = Integer.parseInt(examOne) + Integer.parseInt(examTwo);
+
+        //There are 8 of these wrapper classes, one for each number data type.
+        //https://docs.oracle.com/javase/tutorial/java/data/numberclasses.html
+    }
+}
