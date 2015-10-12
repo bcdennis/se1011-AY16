b1398ec96535a99b04ef6931a2c049272475e7ad
Brad Dennis <bcdennis@gmail.com>
2015-10-12 08:47:34 -0500

    prepping for week 6 lecture

---
+++ src/Bicycle.java
@@ -0,0 +1,18 @@
+/**
+ * Bicycle.java
+ *
+ * This is a simple program that will be used to demonstrate
+ * OOP.
+ *
+ * @author Brad Dennis
+ * Created:  10/12/2015
+ * Modified: --
+ */
+public class Bicycle {
+
+
+    public String details() {
+
+        return "Bike";
+    }
+}
---
+++ src/OOP.java
@@ -0,0 +1,21 @@
+/**
+ * OOP.java
+ *
+ * This is a simple program that will be used to demonstrate
+ * OOP.
+ *
+ * @author Brad Dennis
+ * Created:  10/12/2015
+ * Modified: --
+ */
+public class OOP {
+
+
+    public static void main(String[] args) {
+
+        Bicycle myBike = new Bicycle();
+
+
+        System.out.println(myBike.details());
+    }
+}
