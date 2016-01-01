89dd72f417d691a3b04f80019361009e3b9170e0
bcdennis@gmail.com <bcdennis@gmail.com>
2016-01-01 11:39:30 -0600

    adding in lecture 08 stubs

---
+++ src/JOptionPaneDemo.java
@@ -0,0 +1,16 @@
+/**
+ * JOptionPane.java
+ *
+ * This is a simple program that will be used to demonstrate
+ * more JOptionPane.
+ *
+ * @author Brad Dennis
+ * Created:  1/4/2015
+ * Modified: --
+ */
+public class JOptionPaneDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
